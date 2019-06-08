7c7c82256ac97a570eca4d7299951d096ef5900b
silence <feihu.gao.bj@nikoyo.com>
2019-06-08 14:12:38 +0800

    default commit

--- commitpost.sh
+++ commitpost.sh
@@ -1,7 +1,2 @@
 #! /bin/bash
-cd /Users/silence/Downloads/work/blog/gaofeihu.github.io &
-pwd &
-git pull origin master & 
-git add . & 
-git commit -m  'default commit' &
-git push origin master 
+cd /Users/silence/Downloads/work/blog/gaofeihu.github.io & pwd & git pull origin master & git add . & git commit -m  'default commit' & git push origin master 
